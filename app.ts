408e2434e644c13088f4f525e0dc5b88c8731a4e
jpmgoncalves12 <jpmgoncalves12@gmail.com>
2022-09-24 09:17:32 -0300

    subindo backend

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 const app = express()
-const port = 3000
+const port = 8080
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
