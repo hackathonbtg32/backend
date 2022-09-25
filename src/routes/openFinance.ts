d453911bc7a17470c7d5637c5f22fbf213e68af0
jpmgoncalves12 <jpmgoncalves12@gmail.com>
2022-09-25 18:37:19 -0300

    Ajustando mocks

--- src/routes/openFinance.ts
+++ src/routes/openFinance.ts
@@ -11,16 +11,8 @@ openFinanceRouter.get('/openfinance/brokers/:clientId', (_, response) => {
       customerId: "1850de5a-3cec-11ed-b878-0242ac120002",
       customerName: "Jhon Doe",
       organizationId: "69665991-da55-4aac-a1f2-32d23daba8fe",
-      organizationName: "Banco Roxo",
-      organizationCnpj: "45552164000160",
-      availableBrokerValue: 980
-    },
-    {
-      customerId: "1850de5a-3cec-11ed-b878-0242ac120002",
-      customerName: "Jhon Doe",
-      organizationId: "69665991-da55-4aac-a1f2-32d23daba8fe",
-      organizationName: "XP Investimentos",
-      organizationCnpj: "12498832000169",
+      organizationName: "NOVA XP Investimentos",
+      organizationCnpj: "00394706000185",
       availableBrokerValue: 600
     },
     {
@@ -34,8 +26,16 @@ openFinanceRouter.get('/openfinance/brokers/:clientId', (_, response) => {
     {
       customerId: "1850de5a-3cec-11ed-b878-0242ac120002",
       customerName: "Jhon Doe",
+      organizationId: "a81729b5-ec71-4552-b4b1-26931c130154",
+      organizationName: "Genérica 04",
+      organizationCnpj: "53730309000101",
+      availableBrokerValue: 75
+    },
+    {
+      customerId: "1850de5a-3cec-11ed-b878-0242ac120002",
+      customerName: "Jhon Doe",
       organizationId: "3fda1ef5-4b72-4557-bcbf-296780ba2c1f",
-      organizationName: "Instituição Investimento Genérica 01",
+      organizationName: "Genérica 01",
       organizationCnpj: "37012911000149",
       availableBrokerValue: 5
     },
@@ -43,17 +43,9 @@ openFinanceRouter.get('/openfinance/brokers/:clientId', (_, response) => {
       customerId: "1850de5a-3cec-11ed-b878-0242ac120002",
       customerName: "Jhon Doe",
       organizationId: "e7fc1280-4dd3-4977-bc24-c08ffea8baf5",
-      organizationName: "Instituição Investimento Genérica 06",
+      organizationName: "Genérica 06",
       organizationCnpj: "34125492000163",
       availableBrokerValue: 613
-    },
-    {
-      customerId: "1850de5a-3cec-11ed-b878-0242ac120002",
-      customerName: "Jhon Doe",
-      organizationId: "a81729b5-ec71-4552-b4b1-26931c130154",
-      organizationName: "Instituição Investimento Genérica 04",
-      organizationCnpj: "53730309000101",
-      availableBrokerValue: 75
     }
   ]
 
--- src/useCases/Broker/insertBroker/insertUseCase.ts
+++ src/useCases/Broker/insertBroker/insertUseCase.ts
@@ -17,19 +17,9 @@ export class InsertBrokerUseCase {
         customerId: "1850de5a-3cec-11ed-b878-0242ac120002",
         customerName: "Jhon Doe",
         organizationId: "69665991-da55-4aac-a1f2-32d23daba8fe",
-        organizationName: "Banco Roxo",
-        organizationCnpj: "45552164000160",
-        availableBrokerValue: 980,
-        organizationRole: 'banco'
-      },
-      {
-        customerId: "1850de5a-3cec-11ed-b878-0242ac120002",
-        customerName: "Jhon Doe",
-        organizationId: "69665991-da55-4aac-a1f2-32d23daba8fe",
-        organizationName: "XP Investimentos",
-        organizationCnpj: "12498832000169",
-        availableBrokerValue: 600,
-        organizationRole: 'corretora'
+        organizationName: "NOVA XP Investimentos",
+        organizationCnpj: "00394706000185",
+        availableBrokerValue: 600
       },
       {
         customerId: "1850de5a-3cec-11ed-b878-0242ac120002",
@@ -37,35 +27,31 @@ export class InsertBrokerUseCase {
         organizationId: "69665991-da55-4aac-a1f2-32d23daba8fb",
         organizationName: "Rico",
         organizationCnpj: "84050144000142",
-        availableBrokerValue: 300,
-        organizationRole: 'corretora'
-      },
-      {
-        customerId: "1850de5a-3cec-11ed-b878-0242ac120002",
-        customerName: "Jhon Doe",
-        organizationId: "3fda1ef5-4b72-4557-bcbf-296780ba2c1f",
-        organizationName: "Instituição Investimento Genérica 01",
-        organizationCnpj: "37012911000149",
-        availableBrokerValue: 5,
-        organizationRole: 'corretora'
-      },
-      {
-        customerId: "1850de5a-3cec-11ed-b878-0242ac120002",
-        customerName: "Jhon Doe",
-        organizationId: "e7fc1280-4dd3-4977-bc24-c08ffea8baf5",
-        organizationName: "Instituição Investimento Genérica 06",
-        organizationCnpj: "34125492000163",
-        availableBrokerValue: 613,
-        organizationRole: 'corretora'
+        availableBrokerValue: 300
       },
       {
         customerId: "1850de5a-3cec-11ed-b878-0242ac120002",
         customerName: "Jhon Doe",
         organizationId: "a81729b5-ec71-4552-b4b1-26931c130154",
-        organizationName: "Instituição Investimento Genérica 04",
+        organizationName: "Genérica 04",
         organizationCnpj: "53730309000101",
-        availableBrokerValue: 75,
-        organizationRole: 'corretora'
+        availableBrokerValue: 75
+      },
+      {
+        customerId: "1850de5a-3cec-11ed-b878-0242ac120002",
+        customerName: "Jhon Doe",
+        organizationId: "3fda1ef5-4b72-4557-bcbf-296780ba2c1f",
+        organizationName: "Genérica 01",
+        organizationCnpj: "37012911000149",
+        availableBrokerValue: 5
+      },
+      {
+        customerId: "1850de5a-3cec-11ed-b878-0242ac120002",
+        customerName: "Jhon Doe",
+        organizationId: "e7fc1280-4dd3-4977-bc24-c08ffea8baf5",
+        organizationName: "Genérica 06",
+        organizationCnpj: "34125492000163",
+        availableBrokerValue: 613
       }
     ]
 
